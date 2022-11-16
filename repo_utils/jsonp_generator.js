5b7b766e60b62acd985d7767c4b395aeee4d0674
Francesco Pira <dev@fpira.com>
2022-11-16 20:04:15 +0100

    fixup: actual fix

--- repo_utils/jsonp_generator.js
+++ repo_utils/jsonp_generator.js
@@ -8,6 +8,16 @@ const fs = require('fs')
 const path = require('path')
 const readline = require('readline');
 
+/* constants */
+const targetSubDir = path.join('..', 'jsonp')
+
+/* functions */
+
+function quitOnError(msg, errCode){
+  console.error(msg)
+  process.exit(errCode)
+}
+
 /*
 async function processFileLineByLine(filepath, targetObj) {
   const fileStream = fs.createReadStream(filepath);
@@ -36,30 +46,47 @@ async function processStdinLineByLine(targetObj) {
   })
 }
 
+function createDirIfDoesNotExists(target){
+  try{
+    if (!fs.existsSync(target)){
+      console.log(`Directory ${target} does not exist, creating it...`)
+      fs.mkdirSync(target);
+    } else {
+      console.log(`Directory ${target} already exists, doing nothing...`)
+    }
+  } catch (err) {
+    quitOnError(`Error while trying to creating dir=${target}`, 99)
+  }
+}
+
 function work(what){
   // processLineByLine(`path.resolve(__dirname + `/../${what}.txt`, objs)
   processStdinLineByLine([])
     .then(r => {
       const output = `${what}Callback(` + JSON.stringify(r) + ')'
 
-      const filePath = path.join(__dirname, '..', 'jsonp', `${what}.jsonp`)
+      const targetDir = path.join(__dirname, targetSubDir)
+      createDirIfDoesNotExists(targetDir)
+      const filePath = path.join(targetDir, `${what}.jsonp`)
       // write output to file (overwrites existant with same name)
       fs.writeFile(filePath, output, err => {
-        if (err) console.error(`ERROR while writing to file=${filePath}: ${err.message}`)
-        // else file has been written successfully
-        console.log(`${what} has been processed.\nOutput file: ${filePath}\nOutput written to file: ${output}`)
+        if (err) {
+          quitOnError(`ERROR while writing to file=${filePath}: ${err.message}`, 101)
+        } else {
+          // else file has been written successfully
+          console.log(`${what} processed successfully.\nOutput file: ${filePath}\nOutput written to file: ${output}`)
+        }
       })
     })
     .catch(e => {
-      console.log("ERROR: " + e.message)
+      quitOnError("ERROR: " + e.message, 100)
     })
 }
 
 // script
 const args = process.argv.slice(2);
 if(!args || !args[0]){
-  console.error(`ERROR: missing mandatory param`)
-  process.exit(1)
+  quitOnError(`ERROR: missing mandatory param`, 1)
 }
 switch (args[0]){
   case 'cat':
@@ -71,5 +98,5 @@ switch (args[0]){
     work('tags')
     break;
   default:
-    console.error(`ERROR: unknown option ${args[0]}`)
+    quitOnError(`ERROR: unknown option ${args[0]}`, 2)
 }

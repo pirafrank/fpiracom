75a4b3fe1f433b0a7b80727e9db46d1b1c7e70ff
Francesco Pira <dev@fpira.com>
2025-01-21 10:33:54 +0100

    tiny changes

--- assets/utilities/wapeople.js
+++ assets/utilities/wapeople.js
@@ -1,5 +1,6 @@
 function openChat() {
-  var mobile = document.getElementById('mobile').value;
+  let mobile = document.getElementById('mobile').value;
   document.getElementById("mobile").value = '';
+  mobile = mobile.replace(/^\+/, '');
   window.open('https://api.whatsapp.com/send?phone=' + mobile, "_blank");
 }

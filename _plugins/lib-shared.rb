9aaa3b2d9343f7ea1a785a9a585201b82f03f1c8
Francesco Pira <dev@fpira.com>
2025-02-08 20:55:47 +0100

    plugin refactor: add metadata to post object, update.date as Time object
    - add id and seo_image_url
    - make updates.date a Time object

---
+++ _plugins/lib-shared.rb
@@ -0,0 +1,24 @@
+
+# Convert a date string to a Time object
+def to_time(date_string, site)
+  # assume that the date is in the format YYYY-MM-DD.
+  # performing .to_s to make sure date_string is a string,
+  # cause sometimes it's a Date object..
+  date = DateTime.parse(date_string.to_s)
+  # Get the timezone
+  timezone = site.config['timezone']
+  local_tz = TZInfo::Timezone.get(timezone)
+  # Create a DateTime object at midnight in UTC
+  utc_midnight = DateTime.new(date.year, date.month, date.day, 0, 0, 0, '+00:00')
+  # Convert the UTC DateTime to local timezone
+  local_midnight = local_tz.to_local(utc_midnight)
+  # Set the hour, minute, and second to the local DateTime object
+  h = date.hour || 0
+  m = date.minute || 0
+  s = date.second || 0
+  local = DateTime.new(local_midnight.year, local_midnight.month, local_midnight.day, h, m, s, local_midnight.zone)
+  # returing a Time object as post.date from Jekyll itself
+  return local.to_time
+end
+
+
---
+++ _plugins/post-metadata.rb
@@ -0,0 +1,64 @@
+require 'date'
+require 'tzinfo'
+require_relative 'lib-shared'
+
+module Jekyll
+
+  # This plugin adds metadata to every 'post' object.
+  # It adds:
+  # - a new 'id' attribute,
+  # - a new 'seo_image_url' attribute,
+  # - a new 'most_recent_edit' attribute,
+  # - a new 'most_recent_update' attribute,
+  class PostMetadata < Generator
+    priority :lowest
+
+    def generate(site)
+      site.posts.docs.each do |post|
+
+        # Add the post ID to the post data
+        post_id = post.path
+        post_id = post_id.gsub('_posts/', '')
+        post_id = post_id.gsub('.md', '')
+        post_id = post_id.gsub('.markdown', '')
+        post_id = post_id.gsub('.html', '')
+        post_id = post_id.gsub('.htm', '')
+        post.data['id'] = post_id
+
+        # Add a SEO image to the post data
+        seoimage = ''
+        if post.data['seoimage']
+          seoimage = "#{site.config['url']}#{site.config['baseurl']}/static/postimages/#{post.data['seoimage']}"
+        else
+          seoimage = "#{site.config['url']}#{site.config['baseurl']}/assets/images/og_image.png"
+        end
+        post.data['seo_image_url'] = seoimage
+
+        # Add most_recent_edit and most_recent_update dates to the post data.
+        # It sets most_recent_update to the most recent 'updates.date' if it exists in front-matter,
+        # or to 'post.date' if the former is missing.
+        updates = post.data['updates']
+        if updates && updates.is_a?(Array) && updates.length > 0
+          # sort the updates by date. if there are multiple updates on the same day, sort them by order of appearance
+          # considering that you go appending updates to the end of the array in post front-matter.
+          # note: ordering now because it's a lot harder to do it in liquid.
+          updates.sort_by! { |u| [u['date'], updates.index(u)] }
+          # convert all dates to time objects
+          updates.each { |u| u['date'] = to_time(u['date'], site) }
+          # set to the most recent one
+          latest_update = to_time(updates.last['date'], site)
+          post.data['most_recent_edit'] = latest_update
+          # also, to ease the display of the most recent update in the post,
+          # add the most recent update to the front-matter
+          post.data['most_recent_update'] = latest_update
+        else
+          # if there are no updates, use the creation date of the post
+          post.data['most_recent_edit'] = post.data['date']
+          # and initialize the updates array
+          post.data['updates'] = []
+        end
+      end
+    end
+
+  end
+end
